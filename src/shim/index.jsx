0e822ccf33f383ac8a44a23c23fc553ab4f557d5
anish-work <anish.saxenaa@gmail.com>
2024-04-27 03:16:37 +0530

    shim: introduce shadow root

--- src/shim/index.jsx
+++ src/shim/index.jsx
@@ -13,14 +13,37 @@ export default class GooeyEmbed {
       if (GooeyEmbed.el) {
         throw new Error("GooeyEmbed is already mounted, unmount first");
       }
-      const el = document.getElementById(target_div_id);
-      if (!el) {
-        console.error(
-          `Cannot find element with id ${target_div_id} in DOM`
+      if (customElements.get("gooey-embed-copilot") === undefined) {
+        const template = document.createElement("template");
+        template.innerHTML =`
+        <link href="https://cdn.jsdelivr.net/gh/GooeyAI/gooey-web-widget@1.0.18/dist/style.css" rel="stylesheet" />
+        <div id="gooey-embed" style="height: 100%;" class="gooey-embed-container"></div>
+        `;
+        customElements.define(
+          "gooey-embed-copilot",
+          class extends HTMLElement {
+            constructor() {
+              super();
+              this.attachShadow({ mode: "open" });
+              this.shadowRoot.appendChild(template.content.cloneNode(true));
+            }
+          }
         );
       }
-      el.setAttribute("class", "gooey-embed-container");
-
+      const shadowRoot = document.getElementById(target_div_id).shadowRoot;
+      const embed = shadowRoot.getElementById("gooey-embed");
+      if (document._getElementById === undefined) {
+        document._getElementById = document.getElementById;
+        document.getElementById = (id) => {
+          if (id === "gooey-embed") return embed;
+          return document._getElementById(id);
+        };
+      }
+      const el = document.getElementById("gooey-embed");
+      if (!el) {
+        console.error(`Cannot find element with id ${target_div_id} in DOM`);
+      }
+      shadowRoot.appendChild(embed);
       ReactDOM.render(component, el);
       GooeyEmbed.el = el;
     }
