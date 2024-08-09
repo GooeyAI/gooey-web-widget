4c633e0b1b0982067068f83a827d63e6224caedd
anish-work <anish.saxenaa@gmail.com>
2024-08-10 00:42:52 +0530

    remove un-necessary dependencies

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -4,18 +4,10 @@ import MessagesContextProvider from "src/contexts/MessagesContext";
 import SystemContextProvider from "src/contexts/SystemContext";
 import ChatWidget from "./copilot";
 
-import nunitoStyle500 from "@fontsource/nunito/500.css?inline";
-import nunitoStyle600 from "@fontsource/nunito/600.css?inline";
-import nunitoStyle700 from "@fontsource/nunito/700.css?inline";
-import nunitoStyle from "@fontsource/nunito?inline";
 import { addInlineStyle, Styles } from "src/addStyles";
 import rootStyle from "src/css/root.scss?inline";
 
 addInlineStyle(rootStyle);
-addInlineStyle(nunitoStyle);
-addInlineStyle(nunitoStyle500);
-addInlineStyle(nunitoStyle600);
-addInlineStyle(nunitoStyle700);
 
 export function CopilotChatWidget({ config }: { config?: any }) {
   // apply defaults to the user-provided config
