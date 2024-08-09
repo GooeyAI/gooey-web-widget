8dd9fc52ddee1ed0932bc848abd7c8efa5641d5a
anish-work <anish.saxenaa@gmail.com>
2024-08-09 23:23:49 +0530

    remove unwanted code for home view

--- src/components/shared/Layout/AppLayout.tsx
+++ src/components/shared/Layout/AppLayout.tsx
@@ -1,10 +1,4 @@
 import clsx from "clsx";
-import { useMemo } from "react";
-import HomeIcon from "src/assets/SvgIcons/HomeIcon";
-
-import { addInlineStyle } from "src/addStyles";
-import style from "./appLayout.css?inline";
-addInlineStyle(style);
 
 type Props = {
   children: JSX.Element | JSX.Element[] | (() => JSX.Element);
@@ -12,21 +6,13 @@ type Props = {
   onViewChange?: (val: string) => void;
 };
 
-const getHeightClass = (view: string | undefined) => {
-  if (view === "messages") return "header-height-full";
-  return "header-height-0";
-};
-
 const AppLayout = ({
   children,
-  view,
-  onViewChange = () => undefined,
 }: Props) => {
-  const headerHeightClass = useMemo(() => getHeightClass(view), [view]);
   return (
     <main className="layout-container h-100 d-flex flex-col">
       <div
-        className={clsx("pos-absolute w-100 layout-header", headerHeightClass)}
+        className={clsx("pos-absolute w-100")}
       />
       <>
         <div
@@ -35,31 +21,6 @@ const AppLayout = ({
         >
           <>{children}</>
         </div>
-
-        {/* Nav Footer */}
-        {view === "home" && (
-          <div className="bg-white nav-footer-container w-100 d-flex">
-            <div
-              onClick={() => onViewChange("home")}
-              className={clsx(
-                "d-flex flex-col align-center gpt-12 gpb-12  flex-1 cr-pointer",
-                view === "home" ? "bg-darkGrey text-white" : "hover-bg-primary",
-              )}
-            >
-              <HomeIcon
-                style={{ width: "24px" }}
-                className={clsx(view === "home" && "stroke-white")}
-              />
-              <p className="gmt-6">Home</p>
-            </div>
-            <div
-              onClick={() => onViewChange("messages")}
-              className="d-flex flex-col align-center gpt-12 gpb-12 hover-bg-primary flex-1 cr-pointer"
-            >
-              <p className="gmt-6">Chat</p>
-            </div>
-          </div>
-        )}
       </>
     </main>
   );
