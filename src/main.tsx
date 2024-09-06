0e813a0e61a730fa7d15b58e6a4b1d26948a82c7
anish-work <anish.saxenaa@gmail.com>
2024-09-06 20:38:49 +0530

    copy master main .tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,4 @@
 import GooeyEmbed from "src/lib.tsx";
 
 GooeyEmbed.mount({ target: "#popup", integration_id: "MqL", mode: "popup" });
-GooeyEmbed.mount({
-  target: "#inline",
-  integration_id: "Wdy",
-  mode: "inline",
-});
+GooeyEmbed.mount({ target: "#inline", integration_id: "MqL", mode: "fullscreen" });
