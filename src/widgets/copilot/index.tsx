756347a4332c6445eaf4e647e387a9dc9469ff90
anish-work <anish.saxenaa@gmail.com>
2025-02-19 19:11:45 +0530

    add sentry vite config

--- src/widgets/copilot/index.tsx
+++ src/widgets/copilot/index.tsx
@@ -1,6 +1,22 @@
 import { useSystemContext } from "src/contexts/hooks";
 import CopilotWidget from "./components/widget";
 import WithFabLauncher from "src/components/containers/withFabLauncher";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 export default function ChatWidget() {
   const { config, layoutController } = useSystemContext();
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { peerDependencies } from "./package.json";
 
 export default defineConfig({
@@ -8,12 +9,14 @@ export default defineConfig({
     commonjsOptions: {
       transformMixedEsModules: true,
     },
+
     lib: {
       entry: "./src/lib.tsx", // Specifies the entry point for building the library.
       name: "gooey-chat", // Sets the name of the generated library.
       fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
       formats: ["iife"], // Specifies the output formats (CommonJS and ES modules).
     },
+
     rollupOptions: {
       external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
       preserveEntrySignatures: "strict",
@@ -28,8 +31,12 @@ export default defineConfig({
         assetFileNames: "[name].[ext]", // currently does not work for images
       },
     },
+
     // sourcemap: true, // Generates source maps for debugging.
-    emptyOutDir: true, // Clears the output directory before building.
+    // Clears the output directory before building.
+    emptyOutDir: true,
+
+    sourcemap: true
   },
   define: { "process.env": {} },
   resolve: {
@@ -37,5 +44,11 @@ export default defineConfig({
       src: resolve(__dirname, "src"),
     },
   },
-  plugins: [react()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
+  plugins: [react(), sentryVitePlugin({
+    org: "dara-network",
+    project: "copilot-web-widget"
+  }), sentryVitePlugin({
+    org: "dara-network",
+    project: "copilot-web-widget"
+  })], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
 });
