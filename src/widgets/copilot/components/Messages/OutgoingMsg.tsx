38295cd98f332dda1ae6641b203760f469aeb02c
milovate <cmilind33@gmail.com>
2025-04-29 21:56:18 +0530

    fix: Update iframe positioning and add lazy loading for location map

--- src/widgets/copilot/components/Messages/OutgoingMsg.tsx
+++ src/widgets/copilot/components/Messages/OutgoingMsg.tsx
@@ -59,11 +59,12 @@ const OutgoingMsg = memo((props: any) => {
           {input_prompt}
         </p>
       )}
-      {input_location.latitude && input_location.longitude && (
+      {hasValidLocation && (
         <iframe
           width="100%"
           height="200px"
           src={`https://www.openstreetmap.org/export/embed.html?bbox=${bbox}&layer=mapnik&marker=${input_location.latitude},${input_location.longitude}`}
+          loading="lazy"
           style={{
             border: "1px solid black",
             aspectRatio: "16/9",
