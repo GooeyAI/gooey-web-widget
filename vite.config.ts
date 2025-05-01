6f44b0f6695ce47686041123344b41fdd4e40f13
anish-work <anish.saxenaa@gmail.com>
2025-05-01 20:50:29 +0530

    fix sentry config

--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,56 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { peerDependencies } from "./package.json";
 
-export default defineConfig({
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-
-    lib: {
-      entry: "./src/lib.tsx", // Specifies the entry point for building the library.
-      name: "gooey-chat", // Sets the name of the generated library.
-      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
-      formats: ["iife"], // Specifies the output formats (CommonJS and ES modules).
-    },
-
-    rollupOptions: {
-      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
-      preserveEntrySignatures: "strict",
-      output: {
-        manualChunks: undefined,
-        extend: true,
-        globals: {
-          react: "React",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log(env.SENTRY_AUTH_TOKEN, '>>>>');
+  return {
+    define: { "process.env": {} },
+    build: {
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+  
+      lib: {
+        entry: "./src/lib.tsx", // Specifies the entry point for building the library.
+        name: "gooey-chat", // Sets the name of the generated library.
+        fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
+        formats: ["iife"], // Specifies the output formats (CommonJS and ES modules).
+      },
+  
+      rollupOptions: {
+        external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+        preserveEntrySignatures: "strict",
+        output: {
+          manualChunks: undefined,
+          extend: true,
+          globals: {
+            react: "React",
+          },
+          inlineDynamicImports: true,
+          entryFileNames: "[name].js", // currently does not work for the legacy bundle
+          assetFileNames: "[name].[ext]", // currently does not work for images
         },
-        inlineDynamicImports: true,
-        entryFileNames: "[name].js", // currently does not work for the legacy bundle
-        assetFileNames: "[name].[ext]", // currently does not work for images
+      },
+  
+      // sourcemap: true, // Generates source maps for debugging.
+      // Clears the output directory before building.
+      emptyOutDir: true,
+  
+      sourcemap: true
+    },
+    resolve: {
+      alias: {
+        src: resolve(__dirname, "src"),
       },
     },
-
-    // sourcemap: true, // Generates source maps for debugging.
-    // Clears the output directory before building.
-    emptyOutDir: true,
-
-    sourcemap: true
-  },
-  define: { "process.env": {} },
-  resolve: {
-    alias: {
-      src: resolve(__dirname, "src"),
-    },
-  },
-  plugins: [react(), sentryVitePlugin({
-    org: "dara-network",
-    project: "copilot-web-widget"
-  }), sentryVitePlugin({
-    org: "dara-network",
-    project: "copilot-web-widget"
-  })], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
+    plugins: [react(), sentryVitePlugin({
+      org: "dara-network",
+      project: "copilot-web-widget",
+      authToken: env.SENTRY_AUTH_TOKEN,
+    })],
+  };
 });
