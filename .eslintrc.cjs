93130f33ffa259a54706b41eaec063135afcaab7
anish-work <anish.saxenaa@gmail.com>
2025-05-09 13:57:55 +0530

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,34 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // ⬅️ This is KEY for JSX support
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // ⬅️ Auto-detect React version (fixes many JSX lint errors)
+    },
+  },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended', // ⬅️ This adds React JSX rules
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
+  plugins: ['react', 'react-refresh'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
