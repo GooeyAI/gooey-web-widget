6c35be0bc40f9196926d5f015c47117c5d371110
anish-work <anish.saxenaa@gmail.com>
2025-05-21 16:39:55 +0530

    fix loader animation

--- src/components/shared/Loaders.tsx
+++ src/components/shared/Loaders.tsx
@@ -1,32 +1,6 @@
-export const SkeletonBox = () => {
-  return (
-    <div
-      className="loading-skeleton bg-grey"
-      style={{ width: "100px", height: "100px" }}
-    ></div>
-  );
-};
-
 export const CircularLoader = (props: any) => {
   const size = props.size || 16;
   return (
-    <div className="circular-loader">
-      <svg
-        className="circular"
-        viewBox="25 25 50 50"
-        height={size}
-        width={size}
-      >
-        <circle
-          className="path"
-          cx="50"
-          cy="50"
-          r="20"
-          fill="none"
-          stroke-width="2"
-          stroke-miterlimit="10"
-        ></circle>
-      </svg>
-    </div>
+    <div className="circular-loader" style={{ width: size, height: size }} />
   );
 };
