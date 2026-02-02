94b133eee184e1c63fa357b6ccedd8573d71756f
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2026-02-02 19:39:54 +0530

    add 6px padding on widget container

--- src/widgets/copilot/components/Messages/index.tsx
+++ src/widgets/copilot/components/Messages/index.tsx
@@ -60,6 +60,7 @@ const Messages = () => {
       className={clsx(
         "flex-1 bg-white gpt-16 overflow-y-auto w-100 stable-scroll-container",
       )}
+      style={{ paddingLeft: "6px" }}
     >
       <div
         className="mw-760 d-flex flex-col gpl-8"
