9bb0f2b8901fb0c5e860f993127321e751319dcf
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2026-02-03 19:40:10 +0530

    fix: change import style for fontawesome icons for optimised bundling

--- src/assets/SvgIcons/IconThumbsDown.tsx
+++ src/assets/SvgIcons/IconThumbsDown.tsx
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { byPrefixAndName } from '@awesome.me/kit-c729b8511a/icons';
+import { faThumbsDown } from '@awesome.me/kit-c729b8511a/icons/classic/regular';
 
 const IconThumbsDown = (props: any) => {
   const { size, ...restProps } = props;
   const iconSize = size || 12;
-  return <FontAwesomeIcon icon={byPrefixAndName.far['thumbs-down']} height={iconSize} width={iconSize} {...restProps} />;
+  return <FontAwesomeIcon icon={faThumbsDown} height={iconSize} width={iconSize} {...restProps} />;
 };
 
 export default IconThumbsDown;
--- src/assets/SvgIcons/IconThumbsUp.tsx
+++ src/assets/SvgIcons/IconThumbsUp.tsx
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { byPrefixAndName } from '@awesome.me/kit-c729b8511a/icons';
+import { faThumbsUp } from '@awesome.me/kit-c729b8511a/icons/classic/regular';
 
 const IconThumbsUp = (props: any) => {
   const { size, ...restProps } = props;
   const iconSize = size || 12;
-  return <FontAwesomeIcon icon={byPrefixAndName.far['thumbs-up']} height={iconSize} width={iconSize} {...restProps} />;
+  return <FontAwesomeIcon icon={faThumbsUp} height={iconSize} width={iconSize} {...restProps} />;
 };
 
 export default IconThumbsUp;
