90b2088609ca81a71c0f2cce829940a053cbdcd2
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2026-02-03 19:40:10 +0530

    fix: change import style for fontawesome icons for optimised bundling

--- src/assets/SvgIcons/IconBug.tsx
+++ src/assets/SvgIcons/IconBug.tsx
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { byPrefixAndName } from '@awesome.me/kit-c729b8511a/icons';
+import { faBug } from '@awesome.me/kit-c729b8511a/icons/classic/regular';
 
 const IconBug = (props: any) => {
   const { size, ...restProps } = props;
   const iconSize = size || 12;
-  return <FontAwesomeIcon icon={byPrefixAndName.far.bug} height={iconSize} width={iconSize} {...restProps} />;
+  return <FontAwesomeIcon icon={faBug} height={iconSize} width={iconSize} {...restProps} />;
 };
 
 export default IconBug;
--- src/assets/SvgIcons/IconCopy.tsx
+++ src/assets/SvgIcons/IconCopy.tsx
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { byPrefixAndName } from '@awesome.me/kit-c729b8511a/icons';
+import { faCopy } from '@awesome.me/kit-c729b8511a/icons/classic/regular';
 
 const IconCopy = (props: any) => {
   const { size, ...restProps } = props;
   const iconSize = size || 12;
-  return <FontAwesomeIcon icon={byPrefixAndName.far.copy} height={iconSize} width={iconSize} {...restProps} />;
+  return <FontAwesomeIcon icon={faCopy} height={iconSize} width={iconSize} {...restProps} />;
 };
 
 export default IconCopy;
