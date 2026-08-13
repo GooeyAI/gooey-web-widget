6a9a3aab2a58eb3f1aed65409bd3c82620e44380
Anish <anish.saxenaa@gmail.com>
2026-08-13 12:18:40 +0530

    fix: remove padding from builder and smaller code fonts

--- src/widgets/copilot/components/Messages/IncomingMsg.tsx
+++ src/widgets/copilot/components/Messages/IncomingMsg.tsx
@@ -265,7 +265,7 @@ const IncomingMsg = memo(
       <div className="gooey-incomingMsg gpb-12 mw-100">
         <div
           className={clsx(
-            `gpl-${MESSAGE_GUTTER + 4} gpr-${MESSAGE_GUTTER}`,
+            `gpl-${MESSAGE_GUTTER} gpr-${MESSAGE_GUTTER}`,
             "gooey-incoming-content mw-100",
           )}
         >
